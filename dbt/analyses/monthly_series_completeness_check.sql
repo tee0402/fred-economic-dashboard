9435c823ca209855e1e505983e1c6709af427905
Tony Sio <tonysio42@gmail.com>
2026-05-26 10:55:18 +0000

    Change GROUP BY in completeness analysis

--- dbt/analyses/monthly_series_completeness_check.sql
+++ dbt/analyses/monthly_series_completeness_check.sql
@@ -5,4 +5,4 @@ select
     datediff('month', min(date), max(date)) + 1 as expected_months
 from {{ source('fred', 'fred_observations') }}
 where series_id in ('CIVPART', 'UNRATE', 'CPIAUCSL', 'FEDFUNDS')
-group by series_id
+group by 1
